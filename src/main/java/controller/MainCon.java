7e6dc1289485e2670d1f43d187b4767167f77439
Jaehyun Jun <jaehyunjun627@gmail.com>
2026-02-05 14:30:45 +0900

    인간 다림질(진짜)

--- src/main/java/controller/MainCon.java
+++ src/main/java/controller/MainCon.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.dao.LogDAO;
+import model.LogDAO;
 import model.dto.AccountDTO;
 
 /**
--- src/main/java/model/StudyProgressDTO.java
+++ src/main/java/model/StudyProgressDTO.java
@@ -1,4 +1,4 @@
-package model.dto;
+package model;
 
 import java.util.HashSet;
 import java.util.Set;
